9dbc6bba87475881a7aab24f5f4d839ea8ae1144
swapkothawade <swapnil.kothawade@gmail.com>
2021-01-17 04:54:49 -0500

    nspc initial commit

---
+++ src/main/java/com/nspc/NsPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.nspc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NsPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NsPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nspc/NsPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nspc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NsPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
